343533499b694d08aef92509ffc2ae7590a67b8e
LukaSsS <l.nikiforov@g.nsu.ru>
2021-05-04 00:42:09 +0700

    added всяких приколов

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import telebot
 import pickle
+import os
+from random import randint
+
+def gen_id():
+    return randint(1,2**64)
 
 class Database():
     u_dir="users"
@@ -9,28 +14,46 @@ class Database():
     def __init__(self):
         pass
     def load_all(self):
-        pass
+        for u in sorted(os.listdir(self.u_dir)):
+            with open(self.u_dir+'/'+u, 'rb') as f:
+                self.users.append(pickle.load(f))
+        for b in sorted(os.listdir(self.b_dir)):
+            with open(self.b_dir+'/'+b, 'rb') as f:
+                self.books.append(pickle.load(f))
     def add_book(self,book):
-        pass
+        self.books.append(book)
     def add_user(self,user):
-        pass
-    def dump_all():
-        pass
+        self.users.append(user)
+    def find_books(self,txt):
+        return self.books
+    def remove_book(self, book):
+        os.remove(self.b_dir+'/'+str(book.id))
+        self.books.remove(book)
+    def remove_user(self, user):
+        os.remove(self.u_dir+'/'+str(user.id))
+        self.users.remove(user)
+    def dump_all(self):
+        for u in self.users:
+            with open(str(u.id),'wb') as f:
+                pickle.dump(u,f)
+        for b in self.books:
+            with open(str(b.id),'wb') as f:
+                pickle.dump(b,f)
 
 class User():
+    id=0
     name=""
-    file_name=""
     books=[]
-    def __init__(self,fl):
-        pass
     def __init__(self,name):
-        pass
+        self.id=gen_id()
+        self.name=name
+        self.books=[]
     def add_book(self,book):
-        pass
+        self.books.append(book)
     def remove_book(self,book):
-        pass
+        self.books.remove(book)
     def change_name(self,new_name):
-        pass
+        self.name=new_name
 
 class Stats():
     s_f=""
@@ -46,11 +69,15 @@ class Book():
     name=""
     author=""
     img=""
-    def __init__(self, fl):
-        pass
-    def __init__(self, _name, _author, _img):
-        pass
-    
+    id=0
+    def __init__(self, name, author, img):
+        self.name=name
+        self.author=author
+        self.img=img
+        self.id=gen_id()
+
+
+
 with open("token.secret","r") as f:
     token = f.readline()[:-1]
 
